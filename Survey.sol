6ec94626f60ec719eb438dee2ce9cd21485e7b9c
Qianchen <yuqianchen93@gmail.com>
2018-02-15 14:46:53 +0100

    With factory

--- Survey.sol
+++ Survey.sol
@@ -1,28 +1,35 @@
 pragma solidity ^0.4.17;
 
 /*
-   a simple introduction smart contract
-   with an example of how to set and get values in Solidity
+   SurveyFactory serves as a hub (deployed on the blockchain upon the launching of bEquality)
+   Company can create their own survey by providing a list of permitted user address.
 */
 
 contract SurveyFactory {
 
-  address[] public SurveyContracts;
+  //address owner;
+  mapping(uint => address) public SurveyContracts;
 
-  // useful to know the row count in contracts index
+  // function SurveyFactory(address adr) public {
+  //    owner = adr;
+  //}
 
-  function getContractCount() public constant returns(uint contractCount) {
-    return SurveyContracts.length;
-  }
-
-  // deploy a new contract
-
-  function createNewSurvey(address[] addressessOfEmployees, string _hashToaddressessOfEmployees) public returns(address newContract) {
+  function createNewSurvey(uint companyID, address[] addressessOfEmployees, string _hashToaddressessOfEmployees) public returns(address newContract) {
+    // require(msg.sender == owner);
+    require(SurveyContracts[companyID] == 0x0);
     Survey c = new Survey(addressessOfEmployees, _hashToaddressessOfEmployees);
-    SurveyContracts.push(c);
+    SurveyContracts[companyID] = c;
     return c;
   }
+  
+  function getContractAddress(uint companyID) public constant returns (address) {
+    return SurveyContracts[companyID];
+  }
 }
+
+/*
+   Survey is the child contract created by the SurveyFactory where only the permitted user can modify.
+*/
 
 contract Survey {
     mapping (address => string) public hashes;
@@ -42,5 +49,4 @@ contract Survey {
         hashes[msg.sender] = myHash;
     }
 
-    
 }
