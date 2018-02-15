d52170757fe7bdf2212a7bad1842437bdfb4deed
Qianchen <yuqianchen93@gmail.com>
2018-02-15 14:08:25 +0100

    with contract factory

--- Survey.sol
+++ Survey.sol
@@ -5,6 +5,25 @@ pragma solidity ^0.4.17;
    with an example of how to set and get values in Solidity
 */
 
+contract SurveyFactory {
+
+  address[] public SurveyContracts;
+
+  // useful to know the row count in contracts index
+
+  function getContractCount() public constant returns(uint contractCount) {
+    return SurveyContracts.length;
+  }
+
+  // deploy a new contract
+
+  function createNewSurvey(address[] addressessOfEmployees, string _hashToaddressessOfEmployees) public returns(address newContract) {
+    Survey c = new Survey(addressessOfEmployees, _hashToaddressessOfEmployees);
+    SurveyContracts.push(c);
+    return c;
+  }
+}
+
 contract Survey {
     mapping (address => string) public hashes;
     mapping (address => bool) public isAllowedToSumbitSurvey;
